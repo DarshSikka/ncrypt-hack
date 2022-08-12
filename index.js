02cdb53596abe5fb8a857ca9a5352fd475e2f32d
Darsh Sikka <67545017+DarshSikka@users.noreply.github.com>
2022-08-12 20:36:16 +0530

    remove corsoptions

--- index.js
+++ index.js
@@ -6,22 +6,12 @@ const payments = require("./payments");
 app.use(express.static("public"));
 require("dotenv").config()
 const mongoose = require("mongoose");
-const whitelistedDomains = [process.env.WHITELIST_DOMAIN, "https://syntax-backend-exun.herokuapp.com/"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelistedDomains.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}
 mongoose.connect(process.env.DB_URI, {useUnifiedTopology:true, useNewUrlParser:true}, (error, result)=>{
     console.log("connected");
 })
 const PORT = process.env.PORT || 4000;
 const cors = require("cors");
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/auth", auth);
